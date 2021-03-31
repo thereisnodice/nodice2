fc70e601ada421d664321512d0d306679079a549
Jigsaw <j1g5aw@foxmail.com>
2021-03-31 15:57:40 +0800

    :wrench: Update pyproject.toml & bot.py (using load_from_toml to load plugins)

--- nodice/bot.py
+++ nodice/bot.py
@@ -1,36 +1,14 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import nonebot
 from nonebot.adapters.cqhttp import Bot as CQHTTPBot
 
-# Custom your logger
-# 
-# from nonebot.log import logger, default_format
-# logger.add("error.log",
-#            rotation="00:00",
-#            diagnose=False,
-#            level="ERROR",
-#            format=default_format)
-
-
-# You can pass some keyword args config to init function
 nonebot.init()
 
 app = nonebot.get_asgi()
 
 driver = nonebot.get_driver()
-
 driver.register_adapter("cqhttp", CQHTTPBot)
 
-nonebot.load_plugins("plugins")
-nonebot.load_plugin("nonebot_plugin_manager")
-
-# Modify some config / config depends on loaded configs
-# 
-# config = driver.config
-# do something...
-
+nonebot.load_from_toml("pyproject.toml")
 
 if __name__ == "__main__":
     nonebot.run(app="bot:app")
