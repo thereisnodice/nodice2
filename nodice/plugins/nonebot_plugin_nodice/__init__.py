e94e4a28f2a3aa2ca5459e1d825f1cdec2ac30b6
Jigsaw <j1g5aw@foxmail.com>
2021-04-10 17:40:07 +0800

    :loud_sound: Add log for Adapters

--- nodice/plugins/nonebot_plugin_nodice/__init__.py
+++ nodice/plugins/nonebot_plugin_nodice/__init__.py
@@ -1,7 +1,14 @@
-from nonebot import __version__
+import nonebot
 
-if int(__version__[0]) == 2:
-    from .cqhttp import *
-    from .mirai import *
+if int(nonebot.__version__[0]) == 2:
+    try:
+        from .cqhttp import *
+    except:
+        nonebot.logger.warning("Could not found CQHTTP Adapter !")
+
+    try:
+        from .mirai import *
+    except:
+        nonebot.logger.warning("Could not found MIRAI Adapter !")
 else:
     from .nb import *
