dc8a8df0d02993efb2e025966eb20adf8cc5bda6
Jigsaw <j1g5aw@foxmail.com>
2021-04-11 20:59:01 +0800

    :art: Using fields that comply with Onebot int nodice.db

--- nodice/plugins/nonebot_plugin_nodice/data.py
+++ nodice/plugins/nonebot_plugin_nodice/data.py
@@ -1,52 +1,45 @@
 import sqlite3
 import os
 
-DB_FILE = os.path.join("data", "nodice", "nodice.db")
+_DB_FILE = os.path.join("data", "nodice", "nodice.db")
 
-
-def set_boton(group_id: int, bot_on: bool) -> bool:
-    if insert_db("group_info", {"id": group_id, "bot_on": 1 if bot_on else 0}):
+# 当前角色卡
+def set_current_character(user_id: int, group_id: int, current_character: str) -> bool:
+    if insert_db("group_info", {"user_id": user_id, "group_id": group_id, "current_character": current_character}):
         return True
     else:
-        return update_db("group_info", {"bot_on": 1 if bot_on else 0}, {"id": group_id})
-
-
-def get_boton(group_id: int) -> bool:
-    bot_on = select_db("group_info", ("bot_on",), {"id": group_id})
-    if bot_on[0] is not None:
-        return True if bot_on[0] else False
+        return update_db("group_info", {"current_character": current_character}, {"user_id": user_id, "group_id": group_id})
+def get_current_character(user_id: int, group_id: int) -> str:
+    current_character = select_db("group_info", ("current_character",), {"user_id": user_id, "group_id": group_id})
+    if current_character:
+        return current_character[0]
     else:
-        set_boton(group_id, True)
-        return True
-
+        set_current_character(user_id, group_id, "default")
+        return "default"
 
 # 默认骰
-def set_defaultdice(group_id: int, default_dice: int) -> bool:
-    if insert_db("group_info", {"id": group_id, "default_dice": default_dice}):
+def set_default_dice(group_id: int, default_dice: int) -> bool:
+    if insert_db("group_info", {"group_id": group_id, "default_dice": default_dice}):
         return True
     else:
-        return update_db("group_info", {"default_dice": default_dice}, {"id": group_id})
-
-
-def get_defaultdice(group_id: int) -> int:
-    default_dice = select_db("group_info", ("default_dice",), {"id": group_id})
+        return update_db("group_info", {"default_dice": default_dice}, {"group_id": group_id})
+def get_default_dice(group_id: int) -> int:
+    default_dice = select_db("group_info", ("default_dice",), {"group_id": group_id})
     if default_dice:
         return default_dice[0]
     else:
-        set_defaultdice(group_id, 100)
+        set_default_dice(group_id, 100)
         return 100
 
 
 # 昵称
-def set_nickname(qq_id: int, nickname: str) -> bool:
-    if insert_db("qq_info", {"id": qq_id, "nickname": nickname}):
+def set_nickname(user_id: int, nickname: str) -> bool:
+    if insert_db("user_info", {"user_id": user_id, "nickname": nickname}):
         return True
     else:
-        return update_db("qq_info", {"nickname": nickname}, {"id": qq_id})
-
-
-def get_nickname(qq_id: int, username: str) -> str:
-    nickname = select_db("qq_info", ("nickname",), {"id": qq_id})
+        return update_db("user_info", {"nickname": nickname}, {"user_id": user_id})
+def get_nickname(user_id: int, username: str) -> str:
+    nickname = select_db("user_info", ("nickname",), {"user_id": user_id})
     if nickname:
         return nickname[0]
     else:
@@ -54,25 +47,23 @@ def get_nickname(qq_id: int, username: str) -> str:
 
 
 # 角色卡属性
-def set_property(owner: int, name: str, property: dict) -> bool:
+def set_attribute(user_id: int, name: str, attribute: dict) -> bool:
     if insert_db(
-        "character_card", {"owner": owner, "name": name, "property": str(property)}
+        "character_info", {"user_id": user_id, "name": name, "attribute": str(attribute)}
     ):
         return True
     else:
         return update_db(
-            "character_card",
-            {"property": str(property)},
-            {"owner": owner, "name": name},
+            "character_info",
+            {"attribute": str(attribute)},
+            {"user_id": user_id, "name": name},
         )
-
-
-def get_property(owner: int, name: str):
-    property = select_db(
-        "character_card", ("property",), {"owner": owner, "name": name}
+def get_attribute(user_id: int, name: str):
+    attribute = select_db(
+        "character_info", ("attribute",), {"user_id": user_id, "name": name}
     )
-    if property:
-        return eval(property[0])
+    if attribute:
+        return eval(attribute[0])
     else:
         return {}
 
@@ -86,46 +77,47 @@ def py2sql(value) -> str:
 
 
 def create_db():
-    conn = sqlite3.connect(DB_FILE)
+    conn = sqlite3.connect(_DB_FILE)
     cur = conn.cursor()
     # 用户数据
     # 权限默认 0，黑名单为 -1，骰主为 5，群管在自己群内为 1
     cur.execute(
-        """CREATE TABLE IF NOT EXISTS qq_info (
-                id INTEGER PRIMARY KEY NOT NULL,
+        """CREATE TABLE IF NOT EXISTS user_info (
+                user_id INTEGER PRIMARY KEY NOT NULL,
                 nickname TEXT,
-                permission INTEGER DEFAULT 0,
                 jrrp_value INTEGER,
                 jrrp_date TEXT,
-                card_chosen TEXT DEFAULT \"default\")"""
+                current_character TEXT DEFAULT \"default\"
+            )"""
     )
     # 角色卡
     cur.execute(
-        """CREATE TABLE IF NOT EXISTS character_card (
-                owner INTEGER NOT NULL,
+        """CREATE TABLE IF NOT EXISTS character_info (
+                user_id INTEGER NOT NULL,
                 name TEXT NOT NULL,
-                property TEXT NOT NULL,
-                PRIMARY KEY (owner,name,property))"""
+                attribute TEXT NOT NULL,
+                PRIMARY KEY (user_id,name,attribute)
+            )"""
     )
     # 群数据
     cur.execute(
         """CREATE TABLE IF NOT EXISTS group_info (
-                id INTEGER PRIMARY KEY NOT NULL,
-                bot_on INTEGER DEFAULT 1,
+                group_id INTEGER PRIMARY KEY NOT NULL,
                 help_on INTEGER DEFAULT 1,
                 jrrp_on INTEGER DEFAULT 1,
-                permission INTEGER DEFAULT 0,
                 default_dice INTEGER NOT NULL DEFAULT 100,
-                success_rule INTEGER NOT NULL DEFAULT 0 CHECK (success_rule >= 0 AND success_rule <= 5))"""
+                success_rule INTEGER NOT NULL DEFAULT 0 CHECK (success_rule >= 0 AND success_rule <= 5)
+            )"""
     )
     # 群用户
     cur.execute(
         """CREATE TABLE IF NOT EXISTS group_user_info (
-                qq_id INTEGER NOT NULL,
+                user_id INTEGER NOT NULL,
                 group_id INTEGER NOT NULL,
-                nick_name TEXT,
-                card_chosen TEXT DEFAULT \"default\",
-                PRIMARY KEY(qq_id, group_id))"""
+                nickname TEXT,
+                current_character TEXT DEFAULT \"default\",
+                PRIMARY KEY(user_id, group_id)
+            )"""
     )
     conn.commit()
     conn.close()
@@ -134,7 +126,7 @@ def create_db():
 def update_db(table_name: str, columns: dict, condition: dict) -> bool:
     create_db()
     try:
-        conn = sqlite3.connect(DB_FILE)
+        conn = sqlite3.connect(_DB_FILE)
         cur = conn.cursor()
         sql = f"UPDATE {table_name} SET "
         for i, key in enumerate(columns.keys()):
@@ -157,7 +149,7 @@ def update_db(table_name: str, columns: dict, condition: dict) -> bool:
 def insert_db(table_name: str, columns: dict) -> bool:
     create_db()
     try:
-        conn = sqlite3.connect(DB_FILE)
+        conn = sqlite3.connect(_DB_FILE)
         cur = conn.cursor()
         sql = f"INSERT INTO {table_name} ("
         for i, key in enumerate(columns.keys()):
@@ -181,7 +173,7 @@ def insert_db(table_name: str, columns: dict) -> bool:
 def select_db(table_name: str, columns: tuple, condition: dict):
     create_db()
     try:
-        conn = sqlite3.connect(DB_FILE)
+        conn = sqlite3.connect(_DB_FILE)
         cur = conn.cursor()
         sql = "SELECT "
         for i, value in enumerate(columns):
@@ -203,7 +195,7 @@ def select_db(table_name: str, columns: tuple, condition: dict):
 def delete_db(table_name: str, condition: dict):
     create_db()
     try:
-        conn = sqlite3.connect(DB_FILE)
+        conn = sqlite3.connect(_DB_FILE)
         cur = conn.cursor()
         sql = f"DELETE FROM {table_name} WHERE "
         for i, key in enumerate(condition.keys()):
